01741748fed78bc2cc19b8e9569454ccd099e75c
Moshik Alper <moshikalper@gmail.com>
2025-05-28 14:40:00 +0300

    Add AppHeader component and update app layout
    - Created AppHeader component with HTML and CSS.
    - Replaced placeholder header in app.component.html with AppHeader component.
    - Updated angular.json to remove prefix for the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AppHeaderComponent } from "./cmps/app-header/app-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [AppHeaderComponent],
 })
 export class AppComponent {}
