4e11baa5094e1244d64f049c3c8f81f9a72ba447
Moshik Alper <moshikalper@gmail.com>
2025-05-28 15:15:50 +0300

    Add UserInput component and implement investment calculation form

---
+++ src/app/cmps/user-input/user-input.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'user-input',
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0'
+  enteredAnnualInvestment = '0'
+  enteredExpectedReturn = '0'
+  enteredDuration = '0'
+
+  onSubmit() {
+    console.log('Initial Investment:', this.enteredInitialInvestment);
+    console.log('Annual Investment:', this.enteredAnnualInvestment);
+    console.log('Expected Return:', this.enteredExpectedReturn);
+    console.log('Duration:', this.enteredDuration)
+  }
+}
